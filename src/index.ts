d49c7d569d884396f9dc6d26cce67f3dab7b835c
Tey Han Yee <hytey@techtants.com>
2022-08-04 11:04:49 +0800

    export the necessary add liquidity class and types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {
   Observable as UniswapStream,
-  Subscription as UniswapSubscription,
+  Subscription as UniswapSubscription
 } from 'rxjs';
 export { ErrorCodes } from './common/errors/error-codes';
 export { UniswapError } from './common/errors/uniswap-error';
@@ -12,27 +12,29 @@ export { UniswapVersion } from './enums/uniswap-version';
 export {
   ChainIdAndProvider,
   EthereumProvider,
-  EthersProvider,
+  EthersProvider
 } from './ethers-provider';
-export { GasSettings } from './factories/pair/models/gas-settings';
-export { TradeContext } from './factories/pair/models/trade-context';
-export { LiquidityTradeContext } from './factories/pair/models/liquidity-trade-context';
-export { LiquidityInfoContext, LiquidityInfoContextSingle } from './factories/pair/models/liquidity-info-context';
+export { UniswapAddLiquidityInfoContext } from './factories/main-logics/models/uniswap-add-liquidity-info-context';
 export { UniswapRmLiquidityInfoContext } from './factories/main-logics/models/uniswap-rm-liquidity-info-context';
+export { UniswapAddLiquidity } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-add-liquidity';
+export { UniswapAddLiquidityRatioBased } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-add-liquidity-ratio-based';
+export { UniswapAddRmLiquidityFactory } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-add-rm-liquidity.factory';
+export { UniswapRmLiquidity } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-rm-liquidity';
+export { UniswapMyLiquidity } from './factories/main-logics/uniswap-my-liquidity/uniswap-my-liquidity';
+export { UniswapMyLiquidityFactory } from './factories/main-logics/uniswap-my-liquidity/uniswap-my-liquidity.factory';
+export { UniswapSwap } from './factories/main-logics/uniswap-swap/uniswap-swap';
+export { UniswapSwapFactory } from './factories/main-logics/uniswap-swap/uniswap-swap.factory';
+export { GasSettings } from './factories/pair/models/gas-settings';
+export { LiquidityInfoContext, LiquidityInfoContextSingle } from './factories/pair/models/liquidity-info-context';
+export { LiquidityTradeContext } from './factories/pair/models/liquidity-trade-context';
+export { TradeContext } from './factories/pair/models/trade-context';
 export { TradeDirection } from './factories/pair/models/trade-direction';
 export { Transaction } from './factories/pair/models/transaction';
 export {
   UniswapPairContextForChainId,
-  UniswapPairContextForProviderUrl,
+  UniswapPairContextForProviderUrl
 } from './factories/pair/models/uniswap-pair-contexts';
 export { UniswapPairSettings } from './factories/pair/models/uniswap-pair-settings';
-export { UniswapSwapFactory } from './factories/main-logics/uniswap-swap/uniswap-swap.factory';
-export { UniswapSwap } from './factories/main-logics/uniswap-swap/uniswap-swap';
-export { UniswapAddRmLiquidityFactory } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-add-rm-liquidity.factory';
-export { UniswapAddLiquidity } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-add-liquidity';
-export { UniswapRmLiquidity } from './factories/main-logics/uniswap-add-remove-liquidity/uniswap-rm-liquidity';
-export { UniswapMyLiquidityFactory } from './factories/main-logics/uniswap-my-liquidity/uniswap-my-liquidity.factory';
-export { UniswapMyLiquidity } from './factories/main-logics/uniswap-my-liquidity/uniswap-my-liquidity';
 export { UniswapPairContractFactoryPublicV2 } from './factories/pair/v2/uniswap-pair-contract.factory.public.v2';
 export { RouteQuote } from './factories/router/models/route-quote';
 export { UniswapRouterContractFactoryV2Public } from './factories/router/v2/uniswap-router-contract.factory.public.v2';
@@ -45,3 +47,4 @@ export { TokensFactoryPublic } from './factories/token/tokens.factory.public';
 export { UniswapContractFactoryV2Public } from './factories/uniswap-factory/v2/uniswap-contract.factory.v2.public';
 export { UniswapContractFactoryV3Public } from './factories/uniswap-factory/v3/uniswap-contract.factory.v3.public';
 export { UniswapContractQuoterV3Public } from './factories/uniswap-quoter/v3/uniswap-contract.quoter.v3.public';
+
